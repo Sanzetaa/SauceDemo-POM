d450f2a9239fa1741229330bee2ca4dd1b5539d8
Sanjita Adhikari <sanzeta23@gmail.com>
2026-06-29 09:36:33 +0545

    feat: add inventory page automation

---
+++ pages/inventory_page.py
@@ -0,0 +1,88 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from pages.base_page import BasePage
+
+class InventoryPage(BasePage):
+    URL = "https://www.saucedemo.com/inventory.html"
+
+    #locators 
+    INVENTORY_CONTAINER = (By.ID, "inventory_container")
+    INVENTORY_ITEMS = (By.CLASS_NAME, "inventory_item")
+    APP_LOGO = (By.CLASS_NAME, "app_logo")
+
+    MENU_BUTTON = (By.ID, "react-burger-menu-btn")
+    CLOSE_MENU_BUTTON = (By.ID, "react-burger-cross-btn")
+    LOGOUT_LINK = (By.ID, "logout_sidebar_link")
+    ABOUT_LINK = (By.ID, "about_sidebar_link")
+    RESET_APP_STATE = (By.ID, "reset_sidebar_link")
+
+    CART_ICON = (By.CLASS_NAME, "shopping_cart_link")
+    CART_BADGE = (By.CLASS_NAME, "shopping_cart_badge")
+
+    SORT_DROPDOWN = (By.CLASS_NAME, "product_sort_container")
+
+    ADD_TO_CART_BACKPACK = (By.ID, "add-to-cart-sauce-labs-backpack")
+    REMOVE_BACKPACK = (By.ID, "remove-sauce-labs-backpack")
+
+    ADD_TO_CART_BIKE_LIGHT = (By.ID, "add-to-cart-sauce-labs-bike-light")
+    REMOVE_BIKE_LIGHT = (By.ID, "remove-sauce-labs-bike-light")
+
+    FIRST_PRODUCT = (By.CLASS_NAME, "inventory_item_name")
+
+
+    def load(self):
+        self.open(self.URL)
+        self._wait_for_page_ready()
+
+    def wait_for_inventory_page(self):
+        self.find_visible(self.INVENTORY_CONTAINER)
+
+    def get_page_title(self):
+        return self.find_visible(self.APP_LOGO).text
+
+    def get_all_products(self):
+        return self.find_all_visible(self.INVENTORY_ITEMS)
+
+    def open_menu(self):
+        self.click(self.MENU_BUTTON)
+
+    def close_menu(self):
+        self.click(self.CLOSE_MENU_BUTTON)
+
+    def logout(self):
+        self.open_menu()
+        self.click(self.LOGOUT_LINK)
+
+    def click_about(self):
+        self.open_menu()
+        self.click(self.ABOUT_LINK)
+
+    def reset_app_state(self):
+        self.open_menu()
+        self.click(self.RESET_APP_STATE)
+
+    def open_cart(self):
+        self.click(self.CART_ICON)
+
+    def add_backpack(self):
+        self.click(self.ADD_TO_CART_BACKPACK)
+
+    def remove_backpack(self):
+        self.click(self.REMOVE_BACKPACK)
+
+    def add_bike_light(self):
+        self.click(self.ADD_TO_CART_BIKE_LIGHT)
+
+    def remove_bike_light(self):
+        self.click(self.REMOVE_BIKE_LIGHT)
+
+    def get_cart_badge(self):
+        return self.find_visible(self.CART_BADGE).text
+
+    def click_first_product(self):
+        self.click(self.FIRST_PRODUCT)
+
+    def sort_products(self, option):
+        dropdown = Select(self.find(self.SORT_DROPDOWN))
+        dropdown.select_by_visible_text(option)
+
---
+++ tests/test_inventory.py
@@ -0,0 +1,84 @@
+from pages.login_page import LoginPage
+from pages.inventory_page import InventoryPage
+
+def test_valid_login(driver):
+    login_page = LoginPage(driver)
+    login_page.load()
+    login_page.valid_credential_login("standard_user", "secret_sauce")
+    login_page.wait_for_inventory()
+    assert "inventory" in driver.current_url
+
+def test_inventory_page_loaded(driver):
+    test_valid_login(driver)
+    inventory = InventoryPage(driver)
+    assert "inventory" in driver.current_url
+
+def test_inventory_products_displayed(driver):
+    test_valid_login(driver)
+    inventory = InventoryPage(driver)
+    assert len(inventory.get_all_products()) > 0
+
+def test_add_backpack_to_cart(driver):
+    test_valid_login(driver)
+    inventory = InventoryPage(driver)
+    inventory.add_backpack()
+    assert inventory.get_cart_badge() == "1"
+
+def test_remove_backpack_from_cart(driver):
+    test_valid_login(driver)
+    inventory = InventoryPage(driver)
+    inventory.add_backpack()
+    inventory.remove_backpack()
+
+def test_add_multiple_products(driver):
+    test_valid_login(driver)
+    inventory = InventoryPage(driver)
+    inventory.add_backpack()
+    inventory.add_bike_light()
+    assert inventory.get_cart_badge() == "2"
+
+def test_open_cart(driver):
+    test_valid_login(driver)
+    inventory = InventoryPage(driver)
+    inventory.open_cart()
+    assert "cart" in driver.current_url
+
+def test_sort_name_a_to_z(driver):
+    test_valid_login(driver)
+    inventory = InventoryPage(driver)
+    inventory.sort_products("Name (A to Z)")
+    assert "inventory" in driver.current_url
+
+
+def test_sort_name_z_to_a(driver):
+    test_valid_login(driver)
+    inventory = InventoryPage(driver)
+    inventory.sort_products("Name (Z to A)")
+    assert "inventory" in driver.current_url
+
+def test_sort_price_low_to_high(driver):
+    test_valid_login(driver)
+    inventory = InventoryPage(driver)
+    inventory.sort_products("Price (low to high)")
+    assert "inventory" in driver.current_url
+
+
+def test_sort_price_high_to_low(driver):
+    test_valid_login(driver)
+    inventory = InventoryPage(driver)
+    inventory.sort_products("Price (high to low)")
+    assert "inventory" in driver.current_url
+
+def test_open_first_product(driver):
+    test_valid_login(driver)
+    inventory = InventoryPage(driver)
+    inventory.click_first_product()
+    assert "inventory-item" in driver.current_url
+
+def test_logout(driver):
+    test_valid_login(driver)
+    inventory = InventoryPage(driver)
+    inventory.logout()
+    assert "saucedemo" in driver.current_url
+
+    
