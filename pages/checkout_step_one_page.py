634ac9777417d8606834b7216c7306a5e869e1d2
Sanjita Adhikari <sanzeta23@gmail.com>
2026-06-29 13:31:31 +0545

    feat: add checkout step one page automation

---
+++ pages/checkout_step_one_page.py
@@ -0,0 +1,50 @@
+from selenium.webdriver.common.by import By
+from pages.base_page import BasePage
+
+
+class CheckoutStepOnePage(BasePage):
+    URL = "https://www.saucedemo.com/checkout-step-one.html"
+
+    # Locators
+    CHECKOUT_CONTAINER = (By.ID, "checkout_info_container")
+
+    FIRST_NAME_INPUT = (By.ID, "first-name")
+    LAST_NAME_INPUT = (By.ID, "last-name")
+    POSTAL_CODE_INPUT = (By.ID, "postal-code")
+
+    CONTINUE_BUTTON = (By.ID, "continue")
+    CANCEL_BUTTON = (By.ID, "cancel")
+
+    ERROR_MESSAGE = (By.CSS_SELECTOR, "h3[data-test='error']")
+
+    def wait_for_checkout_page(self):
+        self.find_visible(self.CHECKOUT_CONTAINER)
+
+    def enter_first_name(self, first_name):
+        self.type(self.FIRST_NAME_INPUT, first_name)
+
+    def enter_last_name(self, last_name):
+        self.type(self.LAST_NAME_INPUT, last_name)
+
+    def enter_postal_code(self, postal_code):
+        self.type(self.POSTAL_CODE_INPUT, postal_code)
+
+    def fill_checkout_information(self, first_name, last_name, postal_code):
+        self.enter_first_name(first_name)
+        self.enter_last_name(last_name)
+        self.enter_postal_code(postal_code)
+
+    def click_continue(self):
+        self.click(self.CONTINUE_BUTTON)
+
+    def click_cancel(self):
+        self.click(self.CANCEL_BUTTON)
+
+    def checkout(self, first_name, last_name, postal_code):
+        self.fill_checkout_information(first_name, last_name, postal_code)
+        self.click_continue()
+
+    def get_error_message(self):
+        return self.find_visible(self.ERROR_MESSAGE).text
+    
+
---
+++ tests/test_checkout_step_one.py
@@ -0,0 +1,91 @@
+from pages.login_page import LoginPage
+from pages.inventory_page import InventoryPage
+from pages.cart_page import CartPage
+from pages.checkout_step_one_page import CheckoutStepOnePage
+
+
+def go_to_checkout(driver):
+    login_page = LoginPage(driver)
+    login_page.load()
+    login_page.valid_credential_login("standard_user", "secret_sauce")
+    login_page.wait_for_inventory()
+
+    inventory = InventoryPage(driver)
+    inventory.add_backpack()
+    inventory.open_cart()
+
+    cart = CartPage(driver)
+    cart.checkout()
+
+
+def test_checkout_page_loaded(driver):
+    go_to_checkout(driver)
+
+    checkout = CheckoutStepOnePage(driver)
+    checkout.wait_for_checkout_page()
+
+    assert "checkout-step-one" in driver.current_url
+
+
+def test_valid_checkout_information(driver):
+    go_to_checkout(driver)
+
+    checkout = CheckoutStepOnePage(driver)
+
+    checkout.checkout("Sanjita", "Adhikari", "44600")
+
+    assert "checkout-step-two" in driver.current_url
+
+
+def test_blank_first_name(driver):
+    go_to_checkout(driver)
+
+    checkout = CheckoutStepOnePage(driver)
+
+    checkout.checkout("", "Adhikari", "44600")
+
+    assert "checkout-step-two" not  in  driver.current_url
+
+
+
+def test_blank_last_name(driver):
+    go_to_checkout(driver)
+
+    checkout = CheckoutStepOnePage(driver)
+
+    checkout.checkout("Sanjita", "", "44600")
+
+    assert "checkout-step-two" not in driver.current_url
+
+
+
+def test_blank_postal_code(driver):
+    go_to_checkout(driver)
+
+    checkout = CheckoutStepOnePage(driver)
+
+    checkout.checkout("Sanjita", "Adhikari", "")
+
+    assert "checkout-step-two"  not in driver.current_url
+
+
+
+def test_blank_checkout_information(driver):
+    go_to_checkout(driver)
+
+    checkout = CheckoutStepOnePage(driver)
+
+    checkout.checkout("", "", "")
+
+    assert "checkout-step-two" not in driver.current_url
+
+
+
+def test_cancel_checkout(driver):
+    go_to_checkout(driver)
+
+    checkout = CheckoutStepOnePage(driver)
+
+    checkout.click_cancel()
+
+    assert "cart" in driver.current_url
